8650685e34d77ba7e80b7604866ff80dd6e4f1a4
Reena Mahapatra <reenam2017@gmail.com>
2018-10-14 20:38:13 -0700

    modified the signup page

--- virtual-trainer/client/src/components/sign-up.js
+++ virtual-trainer/client/src/components/sign-up.js
@@ -8,15 +8,8 @@ class Signup extends Component {
       username: "",
       password: "",
       confirmPassword: "",
-      age: "",
-      sex: "",
-      weight: "",
-      height: "",
-      phoneNumber: "",
-      goal: "",
-      recommendedIntake: ""
     };
-    //To keep the value of the input bocxes to always be current with 'state'
+    //To keep the value of the input boxes to always be current with 'state'
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
   }
@@ -40,7 +33,7 @@ class Signup extends Component {
         <h4>Sign Up</h4>
         <form className="form-horizontal">
           <div className="form-group">
-            <div className="col-1 col-ml-auto">
+            <div className="col-md-4 col-sm-12 col-xs-12">
               <label className="form-label" htmlFor="username">
                 Username
               </label>
@@ -53,12 +46,12 @@ class Signup extends Component {
                 name="username"
                 placeholder="username"
                 value={this.state.username}
-                onChange={this.handleInputChange}
+                handleInputChange={this.handleInputChange}
               />
             </div>
-          </div>
-          <div className="row form-group">
-            <div className="col-1 col-ml-auto">
+            {/* </div> */}
+            {/* <div className="row form-group"> */}
+            <div className="col-md-4 col-sm-12 col-xs-12">
               <label className="form-label" htmlFor="password">
                 Password
               </label>
@@ -71,10 +64,10 @@ class Signup extends Component {
                 name="password"
                 placeholder="password"
                 value={this.state.password}
-                onChange={this.handleInputChange}
+                handleInputChange={this.handleInputChange}
               />
             </div>
-            <div className="col-1 col-ml-auto">
+            <div className="col-md-4 col-sm-12 col-xs-12">
               <label className="form-label" htmlFor="confirmPassword">
                 Confirm Password
               </label>
@@ -87,133 +80,7 @@ class Signup extends Component {
                 name="confirmPassword"
                 placeholder="confirm password"
                 value={this.state.confirmPassword}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="age">
-                Age
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <input
-                className="form-input"
-                type="text"
-                id="age"
-                name="age"
-                placeholder="age"
-                value={this.state.age}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="gender">
-                Gender
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <input
-                className="form-input"
-                type="text"
-                id="gender"
-                name="gender"
-                placeholder="gender"
-                value={this.state.gender}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="weight">
-                Weight
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <input
-                className="form-input"
-                type="text"
-                id="weight"
-                name="weight"
-                placeholder="weight(lb)"
-                value={this.state.weight}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="height">
-                Height
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <input
-                className="form-input"
-                type="text"
-                id="height"
-                name="height"
-                placeholder="height(in feet)"
-                value={this.state.height}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="phonenumber">
-                Phone number
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <input
-                className="form-input"
-                type="text"
-                id="phonenumber"
-                name="phonenumber"
-                placeholder="phone number"
-                value={this.state.phonenumber}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="goal">
-                Goal
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <textarea
-                className="form-input"
-                type="text"
-                id="goal"
-                name="goal"
-                placeholder="goal"
-                value={this.state.goal}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="recommendedIntake">
-                Recommended Intake
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <textarea
-                className="form-input"
-                type="text"
-                id="recommendedIntake"
-                name="recommendedIntake"
-                placeholder="recommended intake"
-                value={this.state.recommendedIntake}
-                onChange={this.handleInputChange}
+                handleInputChange={this.handleInputChange}
               />
             </div>
           </div>
